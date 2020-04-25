6c00e3d156d13934f1bf7930fc835a9f6067c26d
fumito80 <fumito80@gmail.com>
2020-04-26 07:10:24 +0900

    add del item

--- superset-frontend/src/test/Form.jsx
+++ superset-frontend/src/test/Form.jsx
@@ -7,6 +7,7 @@ import { getItemById } from './Folder';
 // Actions
 const UPDATE_LABEL = 'UPDATE_LABEL';
 const ADD_ITEM = 'ADD_ITEM';
+const DEL_ITEM = 'DEL_ITEM';
 
 // Action Creators
 function updateLabel(id, label) {
@@ -25,34 +26,56 @@ function addItem(id, label) {
   };
 }
 
+function delItem(id, label) {
+  return {
+    type: DEL_ITEM,
+    id,
+    label,
+  };
+}
+
 // Reducer
 export const updateLabelReducer = {
-  [UPDATE_LABEL]: (state, action) => {
-    const [, path] = getItemById(Number(action.id), state.items);
-    if (!path) {
-      return state;
+  [UPDATE_LABEL]:
+    function(state, action) {
+      const [, path] = getItemById(Number(action.id), state.items);
+      if (!path) {
+        return state;
+      }
+      return _.merge({}, state, { items: _.set({}, [...path, 'label'], action.label) });
+    },
+  [ADD_ITEM]:
+    function(state, action) {
+      const [item, path] = getItemById(Number(action.id), state.items);
+      if (!item) {
+        return state;
+      }
+      const maxId = state.maxId + 1;
+      const children = [...(item.children || []), { id: maxId, label: action.label, type: 'folder' }];
+      return _.merge(
+        {},
+        state,
+        { maxId },
+        { items: _.set({}, [...path, 'children'], children) },
+      );
+    },
+  [DEL_ITEM]:
+    function(state, action) {
+      const [item, path] = getItemById(Number(action.id), state.items);
+      if (!item || path.length === 0) {
+        return state;
+      }
+      const parentPath = path.slice(0, -2);
+      const parent = _.get(state.items, parentPath, state.items);
+      const children = parent.children.filter(item => item.id !== action.id);
+      const newState = _.set(_.merge({}, state), ['items', ...parentPath, 'children'], children);
+      return { ...newState, selectedItem: parent.id };
     }
-    return _.merge({}, state, { items: _.set({}, [...path, 'label'], action.label) });
-  },
-  [ADD_ITEM]: (state, action) => {
-    const [item, path] = getItemById(Number(action.id), state.items);
-    if (!item) {
-      return state;
-    }
-    const maxId = state.maxId + 1;
-    const children = [...(item.children || []), { id: maxId, label: action.label, type: 'folder' }];
-    return _.merge(
-      {},
-      state,
-      { maxId },
-      { items: _.set({}, [...path, 'children'], children) },
-    );
-  }
 }
 
 // Component
 function Form(props) {
-  const { selectedItem, label, onClickUpdate, onClickAdd } = props;
+  const { selectedItem, label, onClickUpdate, onClickAdd, onClickDel } = props;
   let labelInput;
   return (
     <div>
@@ -68,6 +91,7 @@ function Form(props) {
         </label>
         <button onClick={() => onClickUpdate(selectedItem, labelInput.value)}>Alt name</button>
         <button onClick={() => onClickAdd(selectedItem, labelInput.value)}>Add item</button>
+        <button onClick={() => onClickDel(selectedItem, labelInput.value)}>Del item</button>
     </div>
   );
 }
@@ -88,6 +112,9 @@ function mapDispatchToProps(dispatch) {
     },
     onClickAdd(id, label) {
       dispatch(addItem(id, label));
+    },
+    onClickDel(id, label) {
+      dispatch(delItem(id, label));
     },
   };
 }
